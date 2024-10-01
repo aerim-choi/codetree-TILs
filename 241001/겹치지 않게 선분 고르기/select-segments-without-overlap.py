4d11e1d840b3a39a9ba234cef9ffcf0d4cfd623c
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-10-01 22:11:30 +0900

    Title: 겹치지 않게 선분 고르기, Time: 136ms, Memory: 25MB, Status: Wrong Answer - CodeTree

---
+++ 241001/겹치지 않게 선분 고르기/select-segments-without-overlap.py
@@ -0,0 +1,28 @@
+n= int(input())
+
+line = []
+for i in range(n):
+    x1, x2 = map(int, input().split())
+    line.append((x1,x2))
+
+vertical = []
+
+for i in range(0,1001):
+    vertical.append(0)
+
+
+answer= 0 
+for x1,x2 in line:
+
+    flag = True
+    for i in range(x1,x2+1):
+        if vertical[i] == 1: #하나라도 겹치는게 있으면 
+            flag = False
+            break
+    
+    if flag==True:
+        for i in range(x1,x2+1):
+            vertical[i]=1
+        answer+=1
+
+print(answer)
