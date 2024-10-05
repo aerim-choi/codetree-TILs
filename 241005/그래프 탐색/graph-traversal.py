8c840ecb61eb3fdfbec86119b7b01240c43eef27
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-10-05 11:18:09 +0900

    Title: 그래프 탐색, Time: 102ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241005/그래프 탐색/graph-traversal.py
@@ -0,0 +1,24 @@
+def dfs(vertax):
+    for curr_v in graph[vertax]:
+        if visited[curr_v] == 0 :
+            visited[curr_v] = 1
+            dfs(curr_v)
+
+N, M =map(int,input().split())
+graph=[]
+visited = []
+
+for i in range(0,N+1):
+    visited.append(0)
+
+for i in range(0,N+1):
+    graph.append([])
+
+for i in range(M):
+    x,y =map(int,input().split())
+    graph[x].append(y)
+    graph[y].append(x)
+
+dfs(1)
+
+print(sum(visited)-1)
