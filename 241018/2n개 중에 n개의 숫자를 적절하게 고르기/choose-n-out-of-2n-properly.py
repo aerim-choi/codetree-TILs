38b733ff77bf6a63deda4cd531faf975e7c8db9a
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-10-18 18:25:36 +0900

    Title: 2n개 중에 n개의 숫자를 적절하게 고르기, Time: 180ms, Memory: 26MB, Status: Passed - CodeTree

--- 241018/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
+++ 241018/2n개 중에 n개의 숫자를 적절하게 고르기/choose-n-out-of-2n-properly.py
@@ -1,4 +1,4 @@
-n = int(input())
+n = int(input())*2
 
 num_list = list(map(int, input().split()))
 
@@ -10,28 +10,33 @@ def calc(a_list,b_list):
 
 #두 그룹으로 나누기 (N개중에 중복 없이 N/2 뽑기)
 combination_result = []
-combination = []
+combination_idx = []
 calc_answer = 10000 
 def choose(curr_idx, cnt):
-    global combination
+    global combination_idx
     global num_list
     global calc_answer
+    combination1= []
     combination2 = []
-    
-    if cnt > n/2:
-        for i in num_list:
-            if i not in combination:
-                combination2.append(i)
-        calc_answer = min(calc_answer, calc(combination,combination2))
+
+    if cnt == n/2:
+        
+        for i in range(n):
+            if i in combination_idx:
+                combination1.append(num_list[i])
+            else:
+                combination2.append(num_list[i])
+        
+        calc_answer = min(calc_answer, calc(combination1,combination2))
         return
     if curr_idx==len(num_list):
         return 
 
     
     #선택
-    combination.append(num_list[curr_idx])
+    combination_idx.append(curr_idx)
     choose(curr_idx+1, cnt+1)
-    combination.pop()
+    combination_idx.pop()
     
     #선택안함
     choose(curr_idx+1, cnt)
