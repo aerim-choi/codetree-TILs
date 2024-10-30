b53b75207e930d876d00b387162f37b2f2881664
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-10-30 10:22:40 +0900

    Title: 계단 오르기, Time: 122ms, Memory: 25MB, Status: Passed - CodeTree

--- 241030/계단 오르기/climbing-stairs.py
+++ 241030/계단 오르기/climbing-stairs.py
@@ -2,21 +2,27 @@ n = int(input())
 5
 5 -2
 5 -3
+#[-1 -1 -1 -1 1 1 -1 -1]
+memo = [-1 for _ in range(n+1)]
 
-answer = 0
+if n<3:
+    memo[1]=0
+    memo[2]=1
+else:
+    memo[1]=0
+    memo[2]=1
+    memo[3]=1
+
 def dp(n):
-    if n==0:
-        global answer
-        answer+=1
-        return
-    if n<=0:
-        return
-    #3계단 내려가
-    dp(n-3)
-    #2계단 내려가
-    dp(n-2)
+    if memo[n]!=-1:
+        return memo[n]
+    if n<0:
+        return 0
+    
+    memo[n] = dp(n-3) + dp(n-2)
+    return memo[n]
 
 dp(n)
-
 #n층 높이의 계단에 올라가기 위한 서로 다른 방법의 수를 10,007로 나눈 나머지 출력
-print(answer%10007)
+
+print(memo[n]%10007)
