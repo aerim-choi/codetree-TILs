37708638eec3140ce5d63d494ae29528f442a51e
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-10-31 21:27:09 +0900

    Title: 마지막으로 남은 숫자, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 241031/마지막으로 남은 숫자/last-remaining-number.py
@@ -0,0 +1,25 @@
+import heapq
+
+n =int(input())
+
+nums = list(map(int,input().split()))
+max_heap = []
+
+
+for i in range(0,len(nums)):
+    heapq.heappush(max_heap, -nums[i])
+
+#2개 이상 남아 있을 때까지 뽑아
+while len(max_heap)>1:
+    num1 = -heapq.heappop(max_heap)
+    num2 = -heapq.heappop(max_heap)
+
+    diff = abs(num1-num2)
+
+    if diff>0:
+        heapq.heappush(max_heap, -diff)
+    else:
+        continue
+
+#마지막에 남아있는 수를 구하자
+print(-max_heap[0])
