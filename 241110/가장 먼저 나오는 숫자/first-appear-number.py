6dbae3a1994826f5f78152013a7ca4ab3c7580f8
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-11-10 23:56:20 +0900

    Title: 가장 먼저 나오는 숫자, Time: 139ms, Memory: 28MB, Status: Passed - CodeTree

--- 241110/가장 먼저 나오는 숫자/first-appear-number.py
+++ 241110/가장 먼저 나오는 숫자/first-appear-number.py
@@ -5,25 +5,22 @@ arr = list(map(int, input().split()))
 x_arr=list(map(int,input().split()))
 
 def binary_search(x):
-    idx = -1
+    idx = n
     left = 0
     right = len(arr)-1
 
     while left<=right:
         mid = (left+right)//2
         if arr[mid]==x:
-            idx = mid
+            idx = min(mid,idx)
 
         if arr[mid] < x:
             left = mid+1
-
         else:
             right = mid-1
+        
     return idx    
              
 for x in x_arr:
     idx= binary_search(x)
-    if idx==-1:
-        print(-1)
-    else:
-        print(idx+1)
+    print(idx+1 if idx!=n else -1)
