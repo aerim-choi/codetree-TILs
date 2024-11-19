fc7dee86f385fed921282175370a775e4346c888
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-11-19 15:21:40 +0900

    Title: 정원 입장은 선착순, Time: 188ms, Memory: 23MB, Status: Wrong Answer - CodeTree

--- 241119/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
+++ 241119/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -10,9 +10,17 @@ for i in range(n):
 
 wait_time = 0
 curr_time = heap1[0][0]
+wait_time_arr= []
 while heap1:
     a,i,t = heapq.heappop(heap1)
-    curr_time = curr_time + t
+    wait_time = 0
+    if a+t<curr_time:
+        curr_time = curr_time
+        wait_time = curr_time-a
+        wait_time_arr.append(wait_time)
+    else:
+        curr_time=a+t
+        wait_time_arr.append(wait_time)
 
     heap2 = []
     while len(heap1)>0 and heap1[0][0]<=curr_time:
@@ -22,36 +30,11 @@ while heap1:
     
     while heap2:
         i,a,t = heapq.heappop(heap2)
+        
         wait_time=curr_time-a
         curr_time +=t 
-    
+       
+        wait_time_arr.append(wait_time) 
 
-print(wait_time)
-    # 10,4,17
-    # 20,3,50
-    # 25,1,3
-    # 100,5,10
-    # 105,2,30
-
-    # 제일 a가 작은 사람
-    # 10 17
-    # time = 0
-    # curr = 27
-    # 20 3 50
-    # 25 1 3
-
-    # curr =30
-    # time = 2
-
-    # curr = 80
-    # time = 5
-
-    # 100 5 10
-    # curr=100
-    # time=5
-
-    # curr=140
-    # time=10
-
-    
+print(max(wait_time_arr))
 
