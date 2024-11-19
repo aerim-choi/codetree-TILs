e3e305bea65d976ded9f26fd62d65a902ffea2e5
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-11-19 15:47:45 +0900

    Title: 정원 입장은 선착순, Time: 48ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241119/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
+++ 241119/정원 입장은 선착순/admission-to-the-garden-is-on-a-first-come-first-served-basis.py
@@ -18,10 +18,13 @@ while heap1:
         curr_time = curr_time
         wait_time = curr_time-a
         wait_time_arr.append(wait_time)
+        curr_time = curr_time + t
     else:
         curr_time=a+t
         wait_time_arr.append(wait_time)
 
+    print("heap1: a,i,t",a,i,t,wait_time)
+    print("curr_time:",curr_time)
     heap2 = []
     while len(heap1)>0 and heap1[0][0]<=curr_time:
         #순서대로
@@ -33,8 +36,38 @@ while heap1:
         
         wait_time=curr_time-a
         curr_time +=t 
-       
+        print("heap2: a,i,t",a,i,t,wait_time)
+        print("curr_time:",curr_time)
         wait_time_arr.append(wait_time) 
 
-print(max(wait_time_arr))
+print(wait_time_arr)
+    # 10,4,17
+    # 20,3,50
+    # 25,1,3
+    # 100,5,10
+    # 105,2,30
+
+    # 제일 a가 작은 사람
+    # 10 17
+    # time = 0
+    # curr = 27
+    # 20 3 50
+    # 25 1 3
+
+    # curr =30
+    # time = 2
+
+    # curr = 80
+    # time = 5
+
+    # 100 5 10
+    # curr=100
+    # time=5
+
+    # curr=140
+    # time=10
+
+
+
+
 
