878a29a42ab515a64a7330f53fffcc93752ed0e0
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-11-25 18:22:45 +0900

    Title: 특별한 문자, Time: 45ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241125/특별한 문자/special-character.py
@@ -0,0 +1,13 @@
+arr = list(input())
+
+words = dict()
+
+for s in arr:
+    if s in words:
+        words[s]+=1
+    else:
+        words[s]=1
+
+for key, value in words.items():
+    if value==1:
+        print(key, end="")
