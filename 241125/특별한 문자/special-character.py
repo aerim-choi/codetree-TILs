a4d8fc1bf6ca645c635845bced86fb938347d91a
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-11-25 18:26:42 +0900

    Title: 특별한 문자, Time: 56ms, Memory: 21MB, Status: Passed - CodeTree

--- 241125/특별한 문자/special-character.py
+++ 241125/특별한 문자/special-character.py
@@ -8,7 +8,11 @@ for s in arr:
     else:
         words[s]=1
 
+flag = False
 for key, value in words.items():
     if value==1:
         print(key, end="")
+        flag=True
         break
+if flag==False:
+    print(None)
