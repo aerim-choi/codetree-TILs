9bbb7dfd5f38ecb0413090a5edd68b6a17bc43c2
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-11-28 19:06:21 +0900

    Title: 최소 와이파이 수, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 241128/최소 와이파이 수/minimum-number-of-wifi.py
+++ 241128/최소 와이파이 수/minimum-number-of-wifi.py
@@ -2,15 +2,14 @@ n, m =map(int,input().split())
 
 peoples = list(map(int, input().split()))
 
-answer = 0
-k=1
-for i in range(0, len(peoples),k):
-    if peoples[i]==1 and i+m<len(peoples)-1 and peoples[i+m]==1:
-        
-        answer+=1
-        k= i+(2*m+1)
 
+answer = 0
+k=0
+while k<n:
+    if peoples[k]==1:
+        answer+=1
+        k+=(2*m)+1
     else:
-        k=1
-    
+        k+=1
+
 print(answer)
