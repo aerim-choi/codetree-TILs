03a5958f67a6a654f58505f1fc406ff46cd95c17
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-12-01 23:36:36 +0900

    Title: 아름다운 수열 2, Time: 46ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 241201/아름다운 수열 2/beautiful-sequence-2.py
@@ -0,0 +1,21 @@
+n, m =map(int, input().split())
+
+a_arr = list(map(int, input().split()))
+b_arr = list(map(int, input().split()))
+
+#순서가 바뀌면 아름다운 수열
+def is_beautiful_list(arr):
+    b_arr_set = set(b_arr)
+    arr_set = set(arr)
+    if len(b_arr_set-arr_set)==0:
+        return True
+    else:
+        return False
+answer=0
+for i in range(n-m+1):
+    arr = a_arr[i:i+m]
+    
+    if is_beautiful_list(arr):
+        answer+=1
+
+print(answer)
