13f6e985408f765ec1102e0d96e16026c33df1d6
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-12-02 15:01:47 +0900

    Title: 바구니 안의 사탕 2, Time: 92ms, Memory: 18MB, Status: Runtime Error - CodeTree

--- 241202/바구니 안의 사탕 2/candy-in-the-basket-2.py
+++ 241202/바구니 안의 사탕 2/candy-in-the-basket-2.py
@@ -2,27 +2,28 @@ n, k = map(int, input().split()) #n개의 바구니 k구간
 arr= [0 for i in range(101)] #바구니의 위치 0<=바구니위치<=100
 
 def find_candy(start,end):
-    if start<1:
-        start=1
+    if start<0:
+        start=0
     elif end>100:
         end=100
-    
+
     candy_num=0
     for i in range(start,end+1):
         if arr[i]>0:
             candy_num+=arr[i]
-    
+
     return candy_num
         
 
 for _ in range(n):
     candy, basket_location = map(int, input().split())
 
-    arr[basket_location]=candy
+    arr[basket_location] += candy #단, 같은 위치에 여러 바구니가 놓여 있는 것이 가능하므로 +
+
 
 answer= 0 
 #중심점 c찾기
-for i in range(1,101):
+for i in range(0,101):
     answer = max(answer, find_candy(i-k,i+k))
 print(answer)
 
