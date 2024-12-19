fea0b49d54dcb69eae5bd4a49f0fad164b41013b
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-12-19 16:39:55 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 5087ms, Memory: 32MB, Status: Time Limit Exceed - CodeTree

--- 241219/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
+++ 241219/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.py
@@ -4,10 +4,10 @@ car_price_list = list(map(int, input().split()))
 
 answer = 0 #최대이익 
 
-for i in range(len(car_price_list)-1, 0, -1):
-    for j in range(i-1, -1, -1):
-        diff = car_price_list[i]-car_price_list[j]
-   
+for i in range(len(car_price_list)-1, 1, -1):
+        
+        diff = car_price_list[i] - min(car_price_list[0:i-1])
+    
         answer=max(diff, answer)
 
 print(answer)
