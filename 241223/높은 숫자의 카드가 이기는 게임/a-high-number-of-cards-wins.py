01b99fb22b7a6aa124d361d541a8cac06c2918f3
AERIM CHOI <80438964+aerim-choi@users.noreply.github.com>
2024-12-23 14:00:02 +0900

    Title: 높은 숫자의 카드가 이기는 게임, Time: 50ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 241223/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
+++ 241223/높은 숫자의 카드가 이기는 게임/a-high-number-of-cards-wins.py
@@ -12,9 +12,14 @@ for _ in range(n):
     b_cards.append(b_card)
 
 answer = 0 
+b_cards = sorted(b_cards)
 
 for i in range(n):
     if cards[i] > b_cards[i]:
         answer+=1
 
 print(answer)
+
+# 10
+# 1 2 3  4  5  8 10 11 16 19
+# 6 7 9 12 13 14 15 17 18 20
